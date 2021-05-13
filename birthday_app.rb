562f0be3f138122bc2643b8d822534381873386a
Robert Dupplaw <dupplawrobert@gmail.com>
2021-05-13 18:24:30 +0100

    works but could use some refactoring and css

--- birthday_app.rb
+++ birthday_app.rb
@@ -6,6 +6,17 @@ class BirthdayApp < Sinatra::Base
   end
 
   post '/greet' do
-    erb(:greet)
+    @name = params[:name]
+    birthday = params[:birthday]
+    data = birthday.split('-').map(&:to_i)
+    birthday = Date.civil(Date.today.year, data[1], data[2])
+    if birthday == Date.today 
+      erb(:greet)
+    else
+      birthday = Date.civil(Date.today.year + 1, data[1], data[2])
+      @days_until_birthday = (birthday - Date.today).to_i
+      @days_until_birthday = @days_until_birthday > 364 ? @days_until_birthday - 365 : @days_until_birthday
+      erb(:until)
+    end
   end
 end
--- spec/features/birthday_app_spec.rb
+++ spec/features/birthday_app_spec.rb
@@ -8,11 +8,26 @@ feature 'Birthday greeting' do
   context 'when input birthday is today' do
     it 'wishes a happy birthday' do
       today = Date.today.to_s
+
       visit('/')
       fill_in('name', with: 'John')
       fill_in('birthday', with: today)
       click_button('Go!')
+
       expect(page).to have_content('Happy Birthday John!')
+    end
+  end
+
+  context 'when input birthday is not today' do
+    it 'says the number of days until next birthday' do
+      birthday = "1995-04-17"
+
+      visit('/')
+      fill_in('name', with: 'Jen')
+      fill_in('birthday', with: birthday)
+      click_button('Go!')
+
+      expect(page).to have_content('Your birthday will be in 339 days, Jen.')
     end
   end
 end
